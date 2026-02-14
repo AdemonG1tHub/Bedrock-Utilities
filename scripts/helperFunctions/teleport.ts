a642f01e68b63091ac17f3c8d333001f3f8c08bd
adem <adem.usman@icloud.com>
2026-02-14 00:33:07 +0000

    refactor: Update teleportPlayer function to use runTimeout for delayed teleportation

--- scripts/helperFunctions/teleport.ts
+++ scripts/helperFunctions/teleport.ts
@@ -1,9 +1,10 @@
-import { system, world, Player } from "@minecraft/server";
+import { system, Player } from "@minecraft/server";
 
 export function teleportPlayer(player: Player, x: number, y: number, z: number) {
-  let seconds: number = 5;
+  const seconds = 5;
 
   player.sendMessage(`Teleporting in ${seconds} seconds...`);
-  system.waitTicks(seconds * 20);
-  player.teleport({ x, y, z });
+  system.runTimeout(() => {
+    player.teleport({ x, y, z });
+  }, seconds * 20);
 }
